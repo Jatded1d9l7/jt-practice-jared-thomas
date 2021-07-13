bf476f2d734be2835126034267323130ce5b5858
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-13 00:47:27 -0400

    conditionals practice

---
+++ js.practice-conditionals.js
@@ -0,0 +1,97 @@
+// var number1 = 34
+// var number2 = 78
+// // console.log(number1 + " is less than " + number2)
+//
+// var number1 = 34
+// var number2 = 78
+// console.log(`${number1} is less than ${number2}`)
+
+
+// var fruit1 = "orange"
+// var fruit2 = "apple"
+//
+// if(fruit1.length > fruit2.length) {
+//   console.log(`${fruit1} has more letters`)
+// } else if(fruit1.length < fruit2.length) {
+//   console.log(`${fruit2} has more letters`)
+// } else {
+//   console.log("They have the same letters")
+// }
+
+// var number = 9
+//
+// if(number === 100) {
+//   console.log(`${number} is a triple digit number`)
+// } else if(number > 9 && number < 100) {
+//   console.log(`${number} is a double digit number`)
+// } else if(number >= 0 && number <= 9){
+//   console.log(`${number} is a single digit number`)
+// } else {
+//   console.log("please enter a number from 0 to 100")
+// }
+
+// Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+//
+// var item = 100
+// if (item <= 100){
+//   console.log("in budget");
+// }
+
+
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// var hungry = false
+// if (hungry === true){
+//   console.log("eat food")
+// }else{
+//   console.log("keep coding")
+// }
+
+
+// // Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+//
+// var trafficLight = "yellow"
+// if (trafficLight === "green"){
+//   console.log("go")
+// }else if (trafficLight === "yellow"){
+//   console.log("slow down")
+// }else {
+//   console.log("stop")
+// }
+
+
+// Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+
+// var numb1 = 1
+// var numb2 = 2
+//
+// if (numb1 > numb2){
+//   console.log( `${numb1} is larger`)
+// } else if (numb1 < numb2) {
+//   console.log(`${numb2} is larger`)
+// }
+
+
+// Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+//
+// if (numb % 2 == 0) {
+//   console.log(`${numb} is an even number`)
+// } else if (numb % 2 !== 0) {
+//   console.log(`${numb} is an odd number`)
+// } else if (numb === 0) {
+//   console.log(`${numb} is zero`)
+// }
+
+
+// var numb = 1
+//
+// if (numb === 0) {
+//   console.log(`${numb} is zero`)
+// } else if (numb % 2 == 0) {
+//   console.log(`${numb} is an even number`)
+// } else if (numb !== 0) {
+//   console.log(`${numb} is an odd number`)
+// }
