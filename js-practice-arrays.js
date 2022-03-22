866d3a5694e0d6e84e5376d636f2018aa72ee15a
Jared Thomas <jthoma25wv@gmail.comgit config --global user.name Jatded1d9l7>
2022-03-22 12:46:04 -0400

    working through some challenge examples

--- js-practice-arrays.js
+++ js-practice-arrays.js
@@ -26,29 +26,30 @@
 // Challenges
 // Write the code that will add "soda" to the end of the original array.
 
-var groceryList = ["chips", "dip", "cookies"]
-//
-// 
+// var groceryList = ["chips", "dip", "cookies"]
+
+ 
 // console.log(groceryList.push("soda"))
 // console.log(groceryList)
 //
 // // Write the code that will add "granola" to the end of array without altering the original array.
 
-var = updatedList = groceryList.push('Cookies')
-console.log(updatedList)
 
 // var addGranola = ["granola"]
 // console.log(groceryList.concat(addGranola))
-
+// console.log(groceryList)
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
 //
 // var groceryList = ["chips", "dip", "cookies"]
-// //
-// // console.log(groceryList.slice(0, 2))
+// console.log(groceryList.slice(0, 2))
+// console.log(groceryList.slice(0, 2))
 //
 // // Write the code that will add "beans" to the "chips" and "dip" array.
-//
+
+// console.log(groceryList.push("beans", "corn"))
+// console.log(groceryList)
+
 // var groceryList2 = groceryList.slice(0, 2)
 // console.log(groceryList2.push("beans"))
 // console.log(groceryList2)
@@ -56,12 +57,15 @@ console.log(updatedList)
 
 // Write the code that will add the number 0 to the beginning of the array.
 
+// var numbers = [2, 4, 6, 8, 10]
 // console.log(numbers.unshift(0))
 // console.log(numbers)
 
-
-
 // Write the code that will add the number 12 to the end of the array.
+
+
+// console.log(numbers.push(12))
+// console.log(numbers)
 
 // console.log(numbers.push(12))
 // console.log(numbers)
@@ -71,10 +75,34 @@ console.log(updatedList)
 // console.log(numbers.shift())
 // console.log(numbers)
 
-var numbers = [4, 4, 6, 8, 10]
+// console.log(numbers.shift())
+// console.log(numbers)
 
-// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
 
-var numbersX = numbers.slice(0, 5)
-console.log(numbersX.unshift(0))
-console.log(numbersX)
+// var numbers = [4, 4, 6, 8, 10]
+
+// // // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// /// This is a really mentally tricky one. Very interesting.
+
+// var numbersX = numbers.slice(0, 5)
+// console.log(numbersX.unshift(0))
+// console.log(numbersX)
+
+
+// Consider the variable:
+
+// var numSet = [2, 13, 6, 8, 4, 2]
+
+// 9. Write the code that finds the index of the first appearance of the number 2.
+
+// console.log(numSet.indexOf(2))
+
+// 10. Write the code that finds the index of the last appearance of the number 2.
+
+// console.log(numSet.lastIndexOf(2))
+
+// 11. Write the code that returns the number at the third index.
+
+// console.log(numSet[3])
+
+
