ca88f41b94303b9eb04dc76762373b892b40d492
Jared Thomas <jthoma25wv@gmail.comgit config --global user.name Jatded1d9l7>
2022-04-14 14:45:04 -0400

    working working working

--- js-practice-jared.js
+++ js-practice-jared.js
@@ -88,8 +88,8 @@
 
 // console.log("LEARN"[0] === "L")
 
-var theQuestion = "life, the universe, and everything "
-var theAnswer = 42
+// var theQuestion = "life, the universe, and everything "
+// var theAnswer = 42
 
 
 // Write the code that will log theAnswer divided by 2.
@@ -123,3 +123,176 @@ var theAnswer = 42
 // Write the code that will log whether theQuestion.length is greater than theAnswer.
 
 // console.log(theQuestion.length > theAnswer)
+
+
+// const greeting = () => "hello there Jared!"
+// console.log(greeting())
+
+// //or 
+
+// const greeting = () => {
+//     return "Hello there LEARN student!"
+//   }
+
+// const greeting = (name) => {
+//     return `Hello there ${name}!`
+//   }
+//   console.log(greeting("Matt"))
+//   // output --> "Hello there Matt!"
+//   console.log(greeting("Rachael"))
+//   // output --> "Hello there Rachael!"
+
+//   //or
+
+// const greeting = (name) => `Hello there ${name}!`
+
+//   console.log(greeting("Matt"))
+//   // output --> "Hello there Matt!"
+//   console.log(greeting("Rachael"))
+//   // output --> "Hello there Rachael!"
+
+
+//   const multiplier = (num1, num2) => {
+//     return num1 * num2
+//   }
+  
+//   console.log(multiplier(3, 5))
+//   // output --> 15
+//   console.log(multiplier(5, 8))
+//   // output --> 40
+
+//or
+
+//   const multiplier = (num1, num2) => num1 * num2
+
+
+//   console.log(multiplier(3, 5))
+//   // output --> 15
+//   console.log(multiplier(5, 8))
+//   // output --> 40
+
+// create a function called tallEnough that takes in a number called height
+
+// const tallEnough = (height) => {
+//     if(height < 40) {
+//       return "Cannot ride the rollercoaster"
+//     }
+//     else if(height >= 40) {
+//       return "Buckle up, let's ride"
+//     }
+//     else {
+//       return "error"
+//     }
+//   }
+
+// //   console.log(tallEnough(30))
+
+
+//   const tallEnough = (height) => { if(height < 40) {return "Cannot ride the rollercoaster"}
+//       else if(height >= 40) {return "Buckle up, let's ride"}
+//       else {return "error"}}
+
+//   console.log(tallEnough(40))
+
+// var myArr1 = [1, 5, 7, 3, 10]
+
+// const mult5 = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     newArr.push(array[i] * 5)
+//   }
+//   return newArr
+// }
+
+// console.log(mult5(myArr1))
+
+// var myArr2 = [1, 2, 7, 4, 10, 8, 9]
+
+// const onlyEven = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     if(array[i] % 2 === 0){
+//       newArr.push(array[i])
+//     }
+//   }
+//   return newArr
+// }
+
+// console.log(onlyEven(myArr2))
+
+// var myArray = [5, 3, 2, 9, 8]
+
+// const multiplier = myArray.map(value => {
+//     return value * 3
+// })
+
+// console.log(multiplier)
+
+
+// create a function that takes in an array and returns each word capitalized
+// var fruitArray = ["banana", "mango", "apple", "grape"]
+
+// const capitalizer = (array) => {
+//   let eachItem = array.map(value => {
+//     return value[0].toUpperCase() + value.substring(1)
+//   })
+//   return eachItem.join("")
+// }
+// console.log(capitalizer(fruitArray))
+
+
+// create a function that returns only the values at the odd indexes
+// var numbers = [4, 24, 5, 9, 0, 78]
+
+// const getOnlyOddIndex = (array) => {
+//   return array.filter((value, index) => {
+//     return index % 2 !== 0
+//   })
+// }
+// console.log(getOnlyOddIndex(numbers))
+
+
+// var myArr1 = [1, 5, 7, 3, 10]
+
+// const mult5 = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     newArr.push(array[i] * 5)
+//   }
+//   return newArr
+// }
+
+// console.log(mult5(myArr1))
+
+// Write a function that takes in an array and returns a new array with all numbers multiplied by 3.
+// var testArr1 = [3, 9, 15, 4, 10]
+
+// const multiply = (array) => {
+//     let newArr = []
+//     for(let i=0; i<array.length; i++){
+//          newArr.push(array[i] * 3)
+//     }
+//     return newArr
+// }
+
+// console.log(multiply(testArr1))
+
+// Write a function that takes in an array and returns a new array with only odd numbers.
+var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+
+const onlyOdd = (array) => {
+    let odds = []
+        for(let i = 0; i<array.length; i++){
+            if(array[i] % 2 != 0){
+                odds.push(array[i])
+            }
+        }
+        return odds
+    }
+
+    console.log(onlyOdd(testArr2))
+
+// write a function called onlyOdd
+// iterate through the array using for loop or .filture 
+// i % 2 != 0
+
